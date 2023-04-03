70cdc8946b51ccce2179ac22c08d648550b6c369
cheikhouly <49554960+abdoulayecheikhouly@users.noreply.github.com>
2023-04-03 13:58:48 +0000

    udapte dayworker api

--- digital_ferme_back-main/src/main/java/com/projet/ferme/controller/dayworker/DayWorkerController.java
+++ digital_ferme_back-main/src/main/java/com/projet/ferme/controller/dayworker/DayWorkerController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/dayworker")
+@RequestMapping("/api/v1/dayworker")
 public class DayWorkerController {
 
     @Autowired
